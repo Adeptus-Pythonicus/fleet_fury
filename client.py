299052a16c7ff22cb3d5d948298239ad675f3fb3
saksham0601 <saksham0601@gmail.com>
2025-03-20 18:22:28 -0700

    added fastapi from test to test

--- client.py
+++ client.py
@@ -4,30 +4,29 @@ import json
 import websockets
 
 
-async def handle_battleship_connection(websocket):
-    # this is to connect to /client at port 5050
-    async with websockets.connect("ws://127.0.0.1:5050/client") as server_ws:
-
-        # this is a loop? that listens to the battleship
-        # single client connection
-        while True:
-            message = json.loads(await websocket.recv())
-            print(f"Received from battleship.py: {message}")
-
-            # forward the message to server.py
-            await server_ws.send(json.loads(message))
-            # # wait for the reply
-            # response = await server_ws.recv()
-            # print(f"Received from server.py: {response}")
-
-            # relay the response to battleship.py
-            # await websocket.send(json.dumps(response))
-
-
-async def main():
-    # this takes to run the connection to localhost at port 5051 in which battleship.py runs
-    async with websockets.serve(handle_battleship_connection, "localhost", 5051):
-        await asyncio.Future()
+# async def handle_battleship_connection(websocket):
+#     # this is to connect to /client at port 5050
+#     async with websockets.connect("ws://127.0.0.1:5050/client") as server_ws:
+#
+#         # this is a loop? that listens to the battleship
+#         # single client connection
+#         while True:
+#             message = json.loads(await websocket.recv())
+#             print(f"Received from battleship.py: {message}")
+#
+#             # forward the message to server.py
+#             await server_ws.send(json.loads(message))
+#             # # wait for the reply
+#             # response = await server_ws.recv()
+#             # print(f"Received from server.py: {response}")
+#
+#             # relay the response to battleship.py
+#             # await websocket.send(json.dumps(response))
+async def connect_server(target_coords):
+    print(target_coords)
+    # async with websockets.connect("ws://127.0.0.1:5050/client") as server_ws:
+    #     while True:
 
 
-asyncio.run(main())
+async def send_message(message):
+    asyncio.run(connect_server(message))
